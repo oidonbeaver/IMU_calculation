bb50a0b526eda232cc6c3755c3e7b8ae44814b5e
oidon <oidonbeaver@gmail.com>
2021-03-08 02:48:37 +0900

    URL

--- acc_gyro_calc_test.py
+++ acc_gyro_calc_test.py
@@ -1,3 +1,12 @@
+# https://omoroya.com/arduino-lesson32/
+# https://msr-r.net/m5stickc-sdcard-time-interval/
+# https://www.ei.tohoku.ac.jp/xkozima/lab/espTutorial2.html
+# https://shizenkarasuzon.hatenablog.com/entry/2019/02/16/181342
+# https://watako-lab.com/2019/02/28/3axis_gyro/
+# https://shizenkarasuzon.hatenablog.com/entry/2019/02/16/170706
+# https://ahrs.readthedocs.io/en/latest/
+# https://pypi.org/project/AHRS/
+
 #%%
 import numpy as np
 import os
@@ -14,5 +23,8 @@ os.path.dirname(__file__)
 # %%
 folder = "./csv_noise"
 #%%
-file = folder+"/"
-np.loadtxt(file, delimiter=',')
+file = folder+"/IMU_calibrated.csv"
+data=np.loadtxt(file, delimiter=',')
+# %%
+data
+# %%
